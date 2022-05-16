9d19b7156750c6b156503066ba01659a7adf3a08
m7bellei <mateus_xdzin@hotmail.com>
2022-05-16 02:25:50 -0300

    Cors added in server but not configured

--- web/server/src/server.ts
+++ web/server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes';
 
 const app = express();
 
+app.use(cors()) // controle de segurança no backend para que o frontend possa acessar a informação correta
 app.use(express.json());
 app.use(routes);
 
